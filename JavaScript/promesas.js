2814c76b74177b3ed30a3416ebbb98f15b7f43a5
Ivan <vanrock07@gmail.com>
2024-02-16 08:18:15 -0500

    ver3

--- JavaScript/promesas.js
+++ JavaScript/promesas.js
@@ -1,41 +1,44 @@
 //promesas: Espera la respuesta de servicios de terceros  
 //funciones asincronas:
 
-// const peliculas = [
-// {"nombre": "Duro de matar", "año": 1987},
-// {"nombre": "Star Wars","año": 1981},
-// {"nombre": "Pulp Fiction", "año": 1992},
-// {"nombre": "El Quinto Elemento","año": 1996}
+console.log("Promesas");
 
-// ];
+const peliculas = [
+{"nombre": "Duro de matar", "año": 1987},
+{"nombre": "Star Wars","año": 1981},
+{"nombre": "Pulp Fiction", "año": 1992},
+{"nombre": "El Quinto Elemento","año": 1996}
+
+ ];
 // function getPeliculas(){
 //     return peliculas;
 
 // }
-//console.log(getPeliculas())
+// console.log(getPeliculas())
 
-// function getPeliculas(){
-//     return new Promise((resolve, reject) => {
-//         if(peliculas === 0){
-//             reject (new Error("No hay peliculas"))
-//         }
-//         setTimeout(() => {resolve(peliculas);}, 2000);   //tiempo de espera 2 segs       
-//     })
-// }
+function getPeliculas(){
+    return new Promise((resolve, reject) => {
+        if(peliculas === 0){
+            reject (new Error("No hay peliculas"))
+        }
+        setTimeout(() => {resolve(peliculas);}, 2000);   //tiempo de espera 2 segs       
+    })
+}
 // getPeliculas()
 //   .then(peliculas => console.log(peliculas))
 //   .catch(err => console.error(err.message));
 
 
-//  const mostrarPeliculas = async() => {
-//     try{
-//         const peli = await getPeliculas(); //
-//         console.log(peli);
-//     }
-//     catch(err){
-//         console.error(err.message);
-//     }
-//  }
-//  mostrarPeliculas();
 
-print("HOLA")
+//FUNCION ASINCRONA 
+ const mostrarPeliculas = async() => {
+    try{
+        const peli = await getPeliculas(); //
+        console.log(peli);
+    }
+    catch(err){
+        console.error(err.message);
+    }
+ }
+ mostrarPeliculas();
+
