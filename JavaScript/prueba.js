d3b5e2a73ad00548d8e5a1ea385533c930d8354c
Ivan <vanrock07@gmail.com>
2024-02-19 06:54:05 -0500

    ver3

--- JavaScript/prueba.js
+++ JavaScript/prueba.js
@@ -1,7 +1,46 @@
+//callbacks : 
 
- 
+
+// function crearCita (cita, callback) {
+//     var miCita = "como yo siempre digo:" + cita;
+//     callback(miCita);    
+// }
+// function logCita(cita) {
+//     console.log(cita);
+// }
+
+// crearCita(" come tus vegetales", logCita);
 
 
+ //ejemplo2
+
+// function solicitudServidor(consulta, callback) {
+//         setTimeout(function(){
+//             let respuestaServidor = (consulta + "ejecutando");
+//             callback(respuestaServidor)
+//  },2000);
+// }
+// function resultadoServidor(resultado) { 
+//     console.log("Respuesta: " + resultado);
+// }
+
+// solicitudServidor("El servidor está " + resultadoServidor);
+
+//ejemplo 3
+
+function modificar (array, callback) {
+    array.push = ["red"]
+    setTimeout(function(){
+        callback();
+    },1000);
+}
+
+const partes = ["conexion", "caidas", "servidor"];
+
+modificar (partes, function(){
+    console.log(`se modificó el array de ${partes.length} elementos`);
+    console.log(partes);
+})
 
 
 
